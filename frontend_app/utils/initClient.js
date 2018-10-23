1704a6993b200e3a081f5adc6df090521cb91cb1
PersLaunch <personal.launch@gmail.com>
2018-10-23 03:17:15 +0200

    [Frontend - Auth] Middleware Cookie Auth Implemented

--- frontend_app/utils/initClient.js
+++ frontend_app/utils/initClient.js
@@ -1,24 +1,53 @@
 import { createHttpLink } from 'apollo-link-http';
 
-import { ApolloClient } from 'apollo-client';
-import { from } from 'apollo-link';
+import { ApolloClient} from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink, from } from 'apollo-link';
 import fetch from 'isomorphic-fetch'
+import { loadAccessToken } from './cookieUtils'
+import cookie from 'cookie'
+
 
 let apolloClient = null
 
+// Polyfill fetch() on the server (used by apollo-client)
 if (!process.browser) {
   global.fetch = fetch
 }
 
 function _initClient(headers, initialState) {
 
+  // Jamais appelé tant qu'on ne fait pas d'appel GraphQL
+  const authMiddleware = new ApolloLink((operation, forward) => {
+
+    // add the authorization to the headers
+    const cookies = (headers.cookie ? cookie.parse(headers.cookie) : {});  
+    let token = cookies['accesstoken'] || null; // ctx.req.cookies.accesstoken
+  
+    operation.setContext(({ headers = {}}) => ({
+  
+      headers: {
+        ...headers,
+        // Si browser on le prend direct depuis cookie pr car le middleware ne se reactualise pas, donc on veut le dernier token a jour
+        // si SSR on le prend depuis header qui contient cookie du client
+        authorization: "Bearer " + (process.browser ? loadAccessToken() : token),
+      
+      },
+      
+  
+    }));
+    
+    return forward(operation);
+  })
+  
+
   return new ApolloClient({
     initialState,
     link: from([
+      authMiddleware,
       createHttpLink({  
         uri : (process.env.BACKEND_HOST || 'http://localhost') + ':'+ (process.env.BACKEND_PORT || '3001') +'/graphql', 
-        credentials: 'same-origin' 
+        credentials: 'include' // CROSS ORIGIN il faudra héberger le server sur le même domaine que le client.
       }),
     ])
    
