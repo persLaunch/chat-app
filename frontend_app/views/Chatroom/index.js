609d1f150e719bd6f5b20e0738dd88eac444a593
PersLaunch <personal.launch@gmail.com>
2018-10-23 05:15:36 +0200

    [Frontend - GraphicalPage] Chatroom v1

--- frontend_app/views/Chatroom/index.js
+++ frontend_app/views/Chatroom/index.js
@@ -4,6 +4,8 @@ import Loader from '../../components/Loader/Loader'
 import PropTypes from 'prop-types'
 import { graphql } from 'react-apollo'
 
+import Card from '../../components/Card/Card'
+
 class Chatroom extends Component {
 
   static propTypes = {
@@ -21,16 +23,61 @@ class Chatroom extends Component {
     }
 
     console.log(this.props)
+
     return (
-      <div
-        className="container"
-      >
-
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+            <Card style={{ textAlign: 'center' }}>
         
+              <div className="content">
+                {this.props.chatroom.title}
+              </div>
+            </Card>
+          </div>
+        </div>
 
+        <div className="messages-box" >
+          {this.props.chatroom.messages.map((message) => {
+
+            return (
+              <div key={message.id} className="row">
+                <div className="col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+                  <Card  style={{ textAlign: 'center' }}>
+                    <div className="content">
+            
+                      <div >{message}</div>;
+             
+                    </div>
+                  </Card>
+
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <form onSubmit={this.submitForm}>
+
+          <div className="input-group fluid">
+            <input type="text" id="message_input" placeholder="Enter your message" />
+          </div>
+          <button className="button-action"> Send </button>
+        </form>
+     
         <style jsx>{`
-         
-        `}</style>
+        .content {
+          font-size: 20px;
+          font-weight: bolder;
+        }
+
+        .messages-box {
+          height: 300px;
+          background: #FFFFFF;
+
+        }
+   
+
+      `}</style>
       </div>
     )
   }
